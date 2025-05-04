ec45c420c3a7b691d76fdeb10ee3e3a5dc4bd705
Zuwaynib <zainabadeoje@gmail.com>
2025-05-04 18:00:12 +0100

    App.jsx: set up dev environment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <header>
+      <nav>
+        <img src="./logo.svg" alt="Logo" />
+        <h1>Learning React</h1>
+      </nav>
+    </header>
+  )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 
-const user = {
-  userName: "Zuwaynib",
-  age: 19,
-  complexion: "light"
-}
-
-function getUser(user) {
-  return (user.userName + ".  Your age is: " + user.age + ". You are " + user.complexion);
-}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-function tick() {
-  const time = (
-    <div id="div-id">
-      <p>Hello {getUser(user)}</p>
-      <p>Current Time: { new Date().toLocaleTimeString()}</p>
-    </div>
-  );
-
-  root.render(time);
-}
-
-setInterval(tick, 2000);
 
